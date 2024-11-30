5d90256eff9ed2dbe1d78f8f9d2f8f99f0481779
Kira Menshov <kira.mnshv@gmail.com>
2024-11-30 15:26:24 +0200

    logging request to alert

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,9 @@
 import { Hono } from "jsr:@hono/hono";
-import { blocks, modal, slackApi } from "./slack/slack-builders.ts";
 
 const app = new Hono();
 
 app.post("/alert", async (payload) => {
-  const response = await slackApi(
-    "views.open",
-    modal({
-      id: "foodfight-modal",
-      title: "Start a food fight!",
-      trigger_id: payload.trigger_id,
-      blocks: [
-        blocks.section({
-          text: "The discourse demands food drama! *Send in your spiciest food takes so we can all argue about them and feel alive.*",
-        }),
-        blocks.input({
-          id: "opinion",
-          label: "Deposit your controversial food opinions here.",
-          placeholder:
-            "Example: peanut butter and mayonnaise sandwiches are delicious!",
-          initial_value: payload.text ?? "",
-          hint: "What do you believe about food that people find appalling? Say it with your chest!",
-        }),
-        blocks.select({
-          id: "spice_level",
-          label: "How spicy is this opinion?",
-          placeholder: "Select a spice level",
-          options: [
-            { label: "mild", value: "mild" },
-            { label: "medium", value: "medium" },
-            { label: "spicy", value: "spicy" },
-            { label: "nuclear", value: "nuclear" },
-          ],
-        }),
-      ],
-    })
-  );
-
-  if (!response.ok) {
-    console.log(response);
-  }
+  console.log(payload);
 });
 
 Deno.serve(app.fetch);
