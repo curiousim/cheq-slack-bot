98624aca1274ed3f0aeb63fc1aaecdd26bb8ef51
Kira Menshov <kira.mnshv@gmail.com>
2024-11-30 19:43:22 +0200

    depenmdecies settled

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { Hono } from "jsr:@hono/hono";
+import { Hono } from "npm:hono";
+import { prettyJSON } from "npm:hono/pretty-json";
 
 const app = new Hono();
+app.use(prettyJSON());
 
-app.post("/alert", async (payload) => {
-  console.log(payload);
+app.post("/alert", (c) => {
+  console.log(c.req.parseBody());
+
+  return c.json({ message: "Alert received" });
 });
 
 Deno.serve(app.fetch);
